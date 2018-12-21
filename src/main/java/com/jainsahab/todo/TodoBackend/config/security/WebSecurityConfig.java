0b33d37ea94556414d8ed80ed78dd7197d7537c2
Prayas Jain <p.jain3322@gmail.com>
2018-12-21 22:22:49 +0530

    permit signup endpoint

--- src/main/java/com/jainsahab/todo/TodoBackend/config/security/WebSecurityConfig.java
+++ src/main/java/com/jainsahab/todo/TodoBackend/config/security/WebSecurityConfig.java
@@ -1,4 +1,19 @@
 package com.jainsahab.todo.TodoBackend.config.security;
 
-public class WebSecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and().csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/signup").permitAll()
+                .antMatchers(HttpMethod.GET, "/test").permitAll()
+                .anyRequest().authenticated();
+    }
 }
